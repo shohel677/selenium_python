47a4b2f7a044ed61165c580dc4d1d20a696757cc
shohel677 <shohel877@gmail.com>
2025-03-26 23:06:29 +0600

    Assertion library added

--- pages/product_page.py
+++ pages/product_page.py
@@ -34,7 +34,7 @@ class ProductPage(AbstractComponent):
     def product_added_to_cart(self):
         added_number = self.cart_icon.get_text()
         # assert added_number == "1", "Product is not added in cart"
-        AssertionLibrary.should_be_true(added_number == "2", "Product added in cart", "Product is not added in cart")
+        AssertionLibrary.should_be_true(added_number == "1", "Product added in cart", "Product is not added in cart")
 
     def go_to_checkout_page(self):
         self.cart_icon.selenium_click()
